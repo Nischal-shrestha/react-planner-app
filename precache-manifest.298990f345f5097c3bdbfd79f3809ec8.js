eff61bc653610a0fb9fb637d243421d037d93857
Nischal Shrestha <developer.nish@gmail.com>
2018-11-12 20:26:25 +1100

    Updates

---
+++ precache-manifest.298990f345f5097c3bdbfd79f3809ec8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39731d72cbaa5d48a59a",
+    "url": "/react-planner-app/static/js/runtime~main.39731d72.js"
+  },
+  {
+    "revision": "270a238ea99effcb4a04",
+    "url": "/react-planner-app/static/js/main.270a238e.chunk.js"
+  },
+  {
+    "revision": "cca4351797cfaac2be35",
+    "url": "/react-planner-app/static/js/1.cca43517.chunk.js"
+  },
+  {
+    "revision": "270a238ea99effcb4a04",
+    "url": "/react-planner-app/static/css/main.cc9db3b9.chunk.css"
+  },
+  {
+    "revision": "61ae3fd95272b89e19c1296c12f0d0b8",
+    "url": "/react-planner-app/index.html"
+  }
+];
